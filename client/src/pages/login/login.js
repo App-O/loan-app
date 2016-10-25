c9ec7d1e88456a917a1144ec795b5f962fa43029
meg768 <magnus@egelberg.se>
2016-10-25 13:18:58 +0200

    -

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import {Jumbotron, Button, Grid, Row, Col, ListGroup, ListGroupItem, FormControl, ControlLabel, PageHeader} from 'react-bootstrap';
+import {Button, Jumbotron, Grid, Row, Col, ListGroup, ListGroupItem, Form, FormGroup, FormControl, ControlLabel, PageHeader} from 'react-bootstrap';
+import {Modal, Checkbox} from 'react-bootstrap';
 import {sprintf, extend, isString, isObject, isArray, isNumber} from 'yow';
 import {Page} from '../../components/page.js'
+
+var Bootstrap = require( 'react-bootstrap');
 
 require('./login.less');
 
@@ -9,7 +12,8 @@ require('./login.less');
 
 
 
-module.exports = class X extends React.Component {
+
+module.exports = class LoginPage extends React.Component {
 
 
 	constructor(props) {
@@ -18,55 +22,70 @@ module.exports = class X extends React.Component {
 
 		this.state = {};
 		this.state.vars = {};
-		this.onChange = this.onChange.bind(this);
+		this.state.vars.password = '';
+		this.state.vars.email = 'magus@egelberg.se';
+		this.state.vars.foo = 'OLLE';
+
 	};
 
+	login() {
+		console.log(this.state);
+	}
 
-	onChange(event) {
-		var input = parseInt(event.target.value);
 
-		if (isNaN(input))
-			input = '';
+	foo(event) {
+//		var input = parseInt(event.target.value);
+		this.setState({foo:event.target.value});
+	}
 
+	onChange(name, event) {
+		console.log(arguments);
 		var vars = {};
-		vars['username'] = input;
+		vars[name] = event.target.value;
 
-		this.setState({vars:vars})
+		this.setState({vars:extend({}, this.state.vars, vars)})
+
 	}
 
 
 	render() {
 
-		var rootStyle = {};
-		rootStyle.padding  = '1em';
-		rootStyle.position = 'absolute';
-		rootStyle.left     = 0;
-		rootStyle.top      = 0;
-		rootStyle.right    = 0;
-		rootStyle.bottom   = 0;
+		var textBoxStyle = {};
+		//textBoxStyle.fontSize = 'inherit';
+		textBoxStyle.color = 'black';
+
+
 
 		return (
 			<div id='login'>
 				<Page >
 					<Grid>
-						<Row>
-							<Col sm={8} smOffset={2} md={6} mdOffset={3} lg={6}>
+						<Row style={{}}>
+							<Col xs={12} sm={6} smOffset={3}>
 
-								<div style={{borderRadius:'4px',backgroundColor:'rgba(255,255,255,0.9)', padding:'1em', fontSize:'110%'}}>
-									<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lacinia tincidunt nunc a tempor. Praesent.
-									</p>
-									<FormControl  type='text' placeholder='E-post' value={this.state.vars['Omkrets']} onChange={this.onChange} style={{color:'black', fontSize:'inherit'}} >
-									</FormControl>
-									<br/>
-									<FormControl  type='password' placeholder='Lösenord' value={this.state.vars['Omkrets']} onChange={this.onChange} style={{color:'black', fontSize:'inherit'}} >
-									</FormControl>
-									<div style={{padding:'1em', textAlign:'center'}}>
-										<Button href='#' bsStyle="warning" style={{fontSize:'inherit'}}>
+
+								<Form style={{borderRadius:'4px',backgroundColor:'rgba(255,255,255,0.9)', padding:'1em'}}>
+									<FormGroup bsSize='small'>
+										<FormControl.Static>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lacinia tincidunt nunc a tempor. Praesent.
+										</FormControl.Static>
+									</FormGroup>
+
+									<FormGroup bsSize='large'>
+										<FormControl  type='text' placeholder='E-post' value={this.state.vars.email} onChange={this.onChange.bind(this, 'email')} style={textBoxStyle} />
+									</FormGroup>
+									<FormGroup>
+										<FormControl  type='password' label='Lösenord' placeholder='Lösenord' value={this.state.vars.password} onChange={this.onChange.bind(this, 'password')} style={textBoxStyle} />
+										<Checkbox>Kom ihåg mig</Checkbox>
+									</FormGroup>
+									<FormGroup style={{textAlign:'center'}}>
+										<Button onClick={this.login.bind(this)}  style={{}}>
 											Logga in
 										</Button>
-									</div>
-								</div>
+									</FormGroup>
+
+
+								</Form>
 							</Col>
 						</Row>
 					</Grid>
